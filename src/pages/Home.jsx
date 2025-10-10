05bf5b3d7178bee61cb16753b2b52d2e3d309292
Hassan Hammoud <hassanhammoud2000@gmail.com>
2025-10-10 21:27:04 +0300

    setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div className='min-h-screen'>
+      Home
+      {/* ************* THEME TOGGLE ************* */}
+      {/* ************* BACKGROUND EFFECTS ************* */}
+      {/* ************* NAVBAR ************* */}
+      {/* ************* MAIN CONTENT ************* */}
+      {/* ************* FOOTER ************* */}
+    </div>
+  );
+};
+
+export default Home;
